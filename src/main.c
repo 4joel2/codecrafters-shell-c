ecdafa3779ae05d5895cb50d87e057025f8d6d88
Joel Mantik <mantikjoel@gmail.com>
2025-02-24 13:33:23 +0100

    task 3

--- src/main.c
+++ src/main.c
@@ -38,6 +38,7 @@ int main(int argc, char *argv[]) {
     }
     if(!strncmp(input, "type ", 5)) {
       if(check_command_type(input + 5) == 0) {
+        printf("%d\n", check_command_type(input + 5));
         printf("%s is a shell builtin\n", input + 5);
         continue;
       } else {
