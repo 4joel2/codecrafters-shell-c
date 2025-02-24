de58ece48a946b400d150a2bb6178fa38c13996a
Joel Mantik <mantikjoel@gmail.com>
2025-02-24 12:37:59 +0100

    task 1

--- src/main.c
+++ src/main.c
@@ -17,11 +17,12 @@ int main(int argc, char *argv[]) {
     if (len > 0 && input[len - 1] == '\n') {
       input[len - 1] = '\0';
     }
-    printf("%s: command not found\n", input);
-    
     if(input == "exit 0"){
       exit(0);
-    }
+    }       
+    printf("%s: command not found\n", input);
+
+    
   }
   return 0;
 }
